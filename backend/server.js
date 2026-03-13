4ddbe026144585b9eca95f33a7e150649c4b1059
PyNishanth <nishanthbhuvaneswar_py@srmap.edu.in>
2026-03-13 18:57:01 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,6 @@ app.get("/",(req,res)=>{
  res.send("NeoConnect API Running")
 })
 
-app.listen(process.env.PORT,()=>{
- console.log("Server running on port "+process.env.PORT)
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running")
 })
